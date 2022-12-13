57f47fd5c6d71ef29a3123e98d80d16a65eedf0d
Danc-0 <lunjaludaniel@gmail.com>
2022-12-13 14:00:41 +0300

    vehicle ui components done

--- app/src/main/java/com/danc/watchout/presentation/screens/VehicleScreen.kt
+++ app/src/main/java/com/danc/watchout/presentation/screens/VehicleScreen.kt
@@ -1,12 +1,16 @@
 package com.danc.watchout.presentation.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -15,6 +19,7 @@ import androidx.navigation.NavController
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemsIndexed
+import com.danc.watchout.domain.models.VehiclesResult
 import com.danc.watchout.presentation.viewmodels.VehicleViewModel
 
 @Composable
@@ -56,20 +61,18 @@ fun VehicleScreen(navController: NavController) {
                         LazyColumn(
                             content = {
                                 itemsIndexed(vehiclesPage) { index, item ->
-                                    Text(text = "Item index is $index and Item is $item",
-                                        style = TextStyle(
-                                            color = MaterialTheme.colors.primary
-                                        )
-                                    )
+                                    item?.let { vehicleResult ->
+                                        SingleVehicleItem(vehiclesResult = vehicleResult)
+                                    }
                                 }
 
-                                if (vehiclesPage.loadState.append == LoadState.Loading){
+                                if (vehiclesPage.loadState.append == LoadState.Loading) {
                                     item {
                                         Text(text = "Wait. Loading data from the Backend")
                                     }
                                 }
 
-                                if (vehiclesPage.loadState.refresh == LoadState.Loading){
+                                if (vehiclesPage.loadState.refresh == LoadState.Loading) {
                                     item {
                                         CircularProgressIndicator(
                                             modifier = Modifier
@@ -87,3 +90,83 @@ fun VehicleScreen(navController: NavController) {
     )
 }
 
+@Composable
+fun SingleVehicleItem(vehiclesResult: VehiclesResult) {
+    Box(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(width = 1.dp, color = MaterialTheme.colors.onSurface),
+                shape = RoundedCornerShape(20.dp)
+            ),
+        content = {
+            Column(
+                modifier = Modifier.padding(10.dp),
+                content = {
+                    Row(
+                        content = {
+                            Text(
+                                text = "Name: ".uppercase(), style = TextStyle(
+                                    color = MaterialTheme.colors.onBackground
+                                )
+                            )
+                            Text(
+                                text = vehiclesResult.name.uppercase(), style = TextStyle(
+                                    color = MaterialTheme.colors.onBackground,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            )
+                        }
+                    )
+
+                    Row(
+                        content = {
+                            Text(
+                                text = "Created: ".uppercase(), style = TextStyle(
+                                    color = MaterialTheme.colors.onBackground
+                                )
+                            )
+                            Text(
+                                text = vehiclesResult.created, style = TextStyle(
+                                    color = MaterialTheme.colors.onBackground,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            )
+                        }
+                    )
+
+                    Divider(
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .fillMaxWidth(),
+                        color = MaterialTheme.colors.onSurface
+                    )
+
+                    Text(
+                        text = "Features",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+
+                            )
+                    )
+                    SingleFeatureItem("Vehicle Class", feature = vehiclesResult.vehicle_class)
+                    SingleFeatureItem("Vehicle Model", feature = vehiclesResult.model)
+                    SingleFeatureItem("Vehicle Speed", feature = vehiclesResult.max_atmosphering_speed)
+                    SingleFeatureItem("Vehicle Cost", feature = vehiclesResult.cost_in_credits)
+                    SingleFeatureItem("Passengers", feature = vehiclesResult.passengers)
+                }
+            )
+        }
+    )
+}
+
+@Composable
+fun SingleFeatureItem(featureTag: String, feature: String) {
+    Column {
+        Row {
+            Text(text = "${featureTag.uppercase()}: ")
+            Text(text = feature)
+        }
+    }
+}
--- app/src/main/java/com/danc/watchout/presentation/states/FilmState.kt
+++ app/src/main/java/com/danc/watchout/presentation/states/FilmState.kt
@@ -3,6 +3,6 @@ package com.danc.watchout.presentation.states
 import com.danc.watchout.domain.models.SpecificFilmResult
 
 data class FilmState(
-    val specificFilm: ArrayList<SpecificFilmResult>? = null,
+    val specificFilm: List<SpecificFilmResult>? = null,
     val isLoading: Boolean = false
 )
